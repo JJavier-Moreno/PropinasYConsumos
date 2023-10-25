eae378237be8a6cc82bfe973df0ef0c15ebdce86
Jose Javier <jjavier.ferez@gmail.com>
2023-10-25 10:44:36 +0200

    Validación del formulario añadida

--- js/app.js
+++ js/app.js
@@ -7,31 +7,32 @@ let cliente = {
 const btnGuardarCliente = document.querySelector('#guardar-cliente');
 btnGuardarCliente.addEventListener('click', guardarCliente);
 
-function guardarCliente(){
+function guardarCliente() {
 
     const hora = document.querySelector('#hora').value;
     const mesa = document.querySelector('#mesa').value;
 
     const camposVacios = [hora, mesa].some(campo => campo === '');
 
-    if(camposVacios){
+    if (camposVacios) {
         //Comprobar si ya existe una alerta
         const existeAlerta = document.querySelector('.invalid-feedback');
-        if(existeAlerta){
+        if (!existeAlerta) {
+
+            const alerta = document.createElement('DIV');
+            alerta.classList.add('invalid-feedback', 'd-block', 'text-center');
+            alerta.textContent = 'Todos los campos son obligatorios';
+
+            document.querySelector('.modal-body form').appendChild(alerta);
+
+
+            //Eliminar la alerta tras 3 segundos
+            setTimeout(() => {
+                alerta.remove();
+            }, 3000)
+
             return;
         }
-        const alerta = document.createElement('DIV');
-        alerta.classList.add('invalid-feedback','d-block','text-center');
-        alerta.textContent = 'Todos los campos son obligatorios';
-
-        document.querySelector('.modal-body form').appendChild(alerta);
-
-
-        //Eliminar la alerta tras 3 segundos
-        setTimeout(()=>{
-            alerta.remove();
-        },3000)
-    }else{
-        console.log('Todos los campos llenos')
     }
+
 }
