0b9dfe97b06728b6957e0f7b29fea4914130c307
Jose Javier <jjavier.ferez@gmail.com>
2023-10-25 11:15:47 +0200

    Recuperación de los platos de la API y representación de los mismos en el HTML

--- js/app.js
+++ js/app.js
@@ -4,6 +4,12 @@ let cliente = {
     pedido: []
 }
 
+const categorias = {
+    1: 'Comida',
+    2: 'Bebidas',
+    3: 'Postres'
+}
+
 const btnGuardarCliente = document.querySelector('#guardar-cliente');
 btnGuardarCliente.addEventListener('click', guardarCliente);
 
@@ -35,4 +41,66 @@ function guardarCliente() {
         }
     }
 
+    cliente = {...cliente, mesa, hora};
+    
+    //Ocultar Modal
+    const modalFormulario = document.querySelector('#formulario');
+    const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario); //Coge la instancia de formulario
+    modalBootstrap.hide(); //Y ya podemos acceder a uno de los metodos de bootstrap para ocultarlo
+
+    //Mostrar secciones
+    mostrarSecciones();
+
+
+    //Obtener platos de la Api de json-server
+    obtenerPlatos();
+
+}
+
+function mostrarSecciones(){
+    const secciones = document.querySelectorAll('.d-none');
+    secciones.forEach(seccion => {
+        seccion.classList.remove('d-none')
+    })
+}
+
+async function obtenerPlatos(){
+    const url = 'http://localhost:4000/platillos';
+
+    try{
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        mostrarPlatos(resultado);
+
+    }catch(error){
+        console.log(error);
+    }
+}
+
+function mostrarPlatos(platos){
+    
+    const contenido = document.querySelector('#platillos .contenido');
+
+    platos.forEach(plato=> {
+        const row = document.createElement('DIV');
+        row.classList.add('row', 'py-3', 'border-top');
+        
+        const nombre = document.createElement('DIV');
+        nombre.classList.add('col-md-4');
+        nombre.textContent = plato.nombre;
+
+        const precio = document.createElement('DIV');
+        precio.classList.add('col-md-3','fw-bold');
+        precio.textContent = `${plato.precio}€`;
+
+        const categoria = document.createElement('DIV');
+        categoria.classList.add('col-md-3');
+        categoria.textContent = categorias[plato.categoria];
+
+        row.appendChild(nombre);
+        row.appendChild(precio);
+        row.appendChild(categoria);
+
+        contenido.appendChild(row);
+    })
 }
